b74d4e47776d1578f53fbc760cc7eceb8f7af25a
EltonDevOps <elton_de_almeida@hotmail.com>
2021-07-20 21:39:50 -0300

    Add project files.

---
+++ Entities/Department.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula119_121Exercicios.Entities
+{
+    class Department
+    {
+        public string Name { get; set; }
+
+        public Department()
+        {
+
+        }
+        public Department(string name)
+        {
+            Name = name;
+        }
+    }
+}
---
+++ Entities/Enums/WorkerLevel.cs
@@ -0,0 +1,10 @@
+﻿
+namespace Aula119_121Exercicios.Entities.Enums
+{
+    enum WorkerLevel : int
+    {
+        Junior = 0,
+        MidLevel = 1,
+        Senior = 2
+    }
+}
